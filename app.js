565f639b6dc10ff489c68d27d922a566c4cd4fd2
Dipannita <dipannita223@gmail.com>
2020-10-18 20:33:55 +0530

    Environment variable added

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 //jshint esversion:6
-
+require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
-
 const mongoose=require('mongoose');
+const encrypt=require("mongoose-encryption")
+
  // Connection URL 
  mongoose.connect("mongodb://localhost:27017/blogDB",{useNewUrlParser: true, useUnifiedTopology: true }
 );
@@ -14,10 +15,13 @@ const blogSchema={
   title:String,
   content:String
 }
-const userSchema={
+const userSchema=new mongoose.Schema({
   username:String,
   password:String
-}
+})
+
+userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:['password']})
+
 const Blog=mongoose.model("Blog",blogSchema)
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -70,7 +74,7 @@ newUser.save(function(err){
   if(err)
   console.log(err)
   else
-  res.render("welcome")
+  res.redirect("/")
 })
 })
 
