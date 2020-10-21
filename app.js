0e45b5faa5397288e313136908e2263652b8814a
Dipannita <dipannita223@gmail.com>
2020-10-21 14:53:26 +0530

    Security and google Authentication added

--- app.js
+++ app.js
@@ -1,43 +1,22 @@
 //jshint esversion:6
-require('dotenv').config()
+require('dotenv').config();
 const express = require("express");
-
 const https=require("https")
-
+const  findOrCreate = require('mongoose-findorcreate')
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose=require('mongoose');
-const bcrypt=require("bcrypt")
-const saltRounds=10
-
+const session = require('express-session')
+const passport=require("passport")
+const passportLocalMongoose=require("passport-local-mongoose")
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
  // Connection URL 
- mongoose.connect("mongodb://localhost:27017/blogDB",{useNewUrlParser: true, useUnifiedTopology: true }
-);
-
-const blogSchema={
-  title:String,
-  content:String
-}
-const userSchema=new mongoose.Schema({
-  username:String,
-  password:String
-})
-
-
-//AIzaSyD6oJiuEy0e4SuQkuq4mxWwfyfEbmvfhWA
-
-  
-
-
-
 
 //serSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:['password']})
 
-const Blog=mongoose.model("Blog",blogSchema)
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const homeStartingContent = "Home to thousands of stories";
+const aboutContent = "Share your amazing experiences";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
@@ -48,13 +27,119 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
+app.use(session({
+secret:"Our little secret.",
+resave:false,
+saveUninitialised:false
+}))
+app.use(passport.initialize());
+app.use(passport.session())
+
+
+mongoose.connect("mongodb://localhost:27017/blogDB",{useNewUrlParser: true, useUnifiedTopology: true }
+);
+mongoose.set("useCreateIndex",true)
+
+const blogSchema={
+  title:String,
+  content:String
+}
+const userSchema=new mongoose.Schema({
+  username:String,
+  password:String
+})
+userSchema.plugin(passportLocalMongoose)
+
+userSchema.plugin(findOrCreate)
+
+const Blog=mongoose.model("Blog",blogSchema)
+
+
+
+
+
+
+const User= new mongoose.model('User',userSchema)
+passport.use(User.createStrategy())
+passport.serializeUser(function(user,done){
+  done(null,user.id)
+})
+passport.deserializeUser(User.deserializeUser(function(id,done){
+User.findById(id,function(err,user){
+done(err,user)
+})
+}))
+
+
+
+passport.use(new GoogleStrategy({
+  clientID:process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/home",
+  userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
+},
+function(accessToken, refreshToken, profile, cb) {
+  console.log(profile)
+  User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    return cb(err, user);
+  });
+}
+));
+
+
+
+
+
+app.get("/",function(req,res){
+  res.render("welcome")
+    
+})
+
+app.get("/auth/google",
+  passport.authenticate("google",{ scope: ["profile"] }))
+
+app.get("/auth/google/home",
+  passport.authenticate('google', { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect to secrets.
+    res.redirect("/home");
+  });
+
+
+
+
+// app.get("/home",function(req,res){
+//   if(req.isAuthenticated()){
+//     res.render("/")
+//   }
+//   else
+//   res.render("home")
+// })
+
+app.get("/home", function(req, res){
+  
   Blog.find({},function(err,article){
-    if(!err){
+    if(err){
+      
+     console.log(err)
+    }
+      else
+      if(req.isAuthenticated()){
+        res.render("/")
+      }
+      else
+      {
+        if(article)
       res.render("home", {startingContent: homeStartingContent,posts:article})
     }
-  });
+  })
+
+
 });
+app.get("/logout",function(req,res){
+  req.logout();
+  res.redirect("/");
+})
 app.get("/login",function(req,res){
   res.render("login")
 })
@@ -85,61 +170,47 @@ app.get("/register",function(req,res){
  
 })
 app.get("/compose", function(req, res){
-  res.render("compose");
-  
+  if(req.isAuthenticated())
+  res.redirect("/");
+  else
+  res.render("compose")
 });
 
 
-const User= new mongoose.model('User',userSchema)
+
 
 app.post("/register",function(req,res){
-  bcrypt.hash(req.body.password,saltRounds,function(err,hash){
-    if(!err){
-    const newUser=new User({
-      username:req.body.email,
-      password:hash
-     
-    })
-    
-    newUser.save(function(err,data){
-      console.log(data)
-      if(err)
+  User.register({username:req.body.username},req.body.password,function(err,user){
+    if(err){
       console.log(err)
-      else
-      res.redirect("/")
+      res.redirect("/register")
+    }
+    else
+    passport.authenticate("local")(req,res,function(){
+      res.render("home")
     })
-  }
-  else
-  console.log(err)
   })
-  //after hashing use it in mongoose document
+
+
 
 })
 
 app.post('/login',function(req,res){
-  let username=req.body.email
-  let password=(req.body.password)
-  User.findOne({username:username},function(err,foundUser){
-    if(err){
-      console.log(err)
-    } 
-    console.log(foundUser)
-    if(foundUser){
-      bcrypt.compare(password, foundUser.password, function(err, result) {
-        // result == true
-        if(result===true)
-        res.redirect("/")
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
 
-    });
-  }
-      else
-      {
-      res.render("register")
-      }
-    
-  })
+  req.login(user, function(err){
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        res.render("home");
+      });
+    }
+  });
 })
-
 
 
 app.post("/compose", function(req, res){
@@ -171,6 +242,6 @@ app.get("/posts/:postName", function(req, res){
  
 });
 
-app.listen(3001, function() {
-  console.log("Server started on port 3001");
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 })
