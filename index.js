fd2d664e2dc131b442682d76757818e47075120e
Dipannita <dipannita223@gmail.com>
2021-01-10 10:56:38 +0530

    Blog

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ let x="" ;let y="";
 passport.use(new GoogleStrategy({
   clientID:process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/home",
+ callbackURL: "http://localhost:3000/auth/google/home",
   userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
 },
 function(accessToken, refreshToken, profile, cb) {
